98c64f20fe425dab5be1d21879e01991ce70a08f
Truong Vuong <truong.vuong@ekoios.vn>
2022-10-04 01:02:55 +0700

    update design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,222 @@
 
-import './App.css';
+import './App.scss';
+import React, { Component }  from 'react';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
+   <div className="App">
+                <nav className="navbar app">App Bar</nav>
+            <nav className="navbar board">Board Bar</nav>
+            <div className="board-columns">
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className="column">
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img src="https://picsum.photos/200" alt="" />
+                            <div className="title">Title: TRUNGQUANDEV</div>
+                        </li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                        <li>Add what you'd like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+            </div>
+
+   </div>
+   
   );
 }
 
